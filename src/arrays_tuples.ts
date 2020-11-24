b3d3c56bf4cf3c8665871d3779ebf94bc1fb8235
Aleksey Safonov <alekseysa@playtika.com>
2020-11-24 19:54:39 +0300

    add arrays sample

---
+++ src/arrays_tuples.ts
@@ -0,0 +1,11 @@
+
+// array
+let number_array: number[];
+let number_and_string_array: (number | string)[];
+let empty_array: number[] = [];
+
+// tuples
+let tuple: [string, number];
+tuple = ["tttt", 676];
+let str = tuple[0];
+let numb = tuple[1];
