034f43bf43aee70b87741fcf387a506012ab43bf
Aleksey Safonov <alekseysa@playtika.com>
2020-12-01 09:32:57 +0300

    update function samples

---
+++ src/functions_overloadings.ts
@@ -0,0 +1,22 @@
+type Reservation = {
+    from: Date;
+    toOrDestination: Date | string;
+    status: string;
+}
+
+// Полный вариант объявления функции.
+type Reserve = {
+    (from: Date, to: Date, destinations: string) : Reservation;
+    (from: Date, destination: string) : Reservation;
+}
+
+let reserve: Reserve = (from, toOrDestination: Date | string) => {
+    
+    let result: Reservation = {
+        from: from,
+        toOrDestination: toOrDestination,
+        status: "approved"
+    }
+
+    return result;
+}
